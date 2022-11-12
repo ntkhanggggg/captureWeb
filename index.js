c0ea2a95b1fb32418bfb53ed9c20036340233d31
ntkhanggggg <84844986+ntkhanggggg@users.noreply.github.com>
2022-11-12 11:08:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const fs = require('fs-extra');
-var bodyParser = require('body-parser')
-
+const express = require("express");
+const puppeteer = require("puppeteer");
+const fs = require("fs-extra");
+const bodyParser = require("body-parser");
 
 const app = express();
 // app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log('listening at ' + PORT));
-app.get('/', (req, res) => res.send("Hi"));
+app.listen(PORT, () => console.log("listening at " + PORT));
+app.get("/", (req, res) => res.send("Hi"));
 // create api capture web page
-app.post('/api/capture', async (req, res) => {
+app.post("/api/capture", async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const browser = await puppeteer.launch({
-      headless: true, args: ['--no-sandbox']
+      headless: true,
+      args: ["--no-sandbox"]
     });
     const url = req.body.url;
     let cookies = req.body.cookies;
     const page = await browser.newPage();
     await page.setViewport({
-      width: 1920, height: 1080
+      width: 1920,
+      height: 1080
     });
-    if (cookies)
-      await page.setCookie(...(JSON.parse(cookies)));
+    if (cookies) await page.setCookie(...JSON.parse(cookies));
     await page.goto(url);
     const pathSave = __dirname + "/image" + Date.now() + ".png";
     await page.screenshot({
       path: pathSave
     });
-    
+
     res.sendFile(pathSave);
-    res.on('finish', () => {
+    res.on("finish", () => {
       fs.unlink(pathSave, () => {});
     });
-await browser.close();
-
-
-
-  }
-  catch(err) {
+    await browser.close();
+  } catch (err) {
+		console.log(err);
     res.status(500).send({
       error: err.name,
       message: err.message
-    })
+    });
   }
 });
